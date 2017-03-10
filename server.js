d07b1c0f84add7759eb0c61845226bafaa61b000
Eric Sayler <easayler@gmail.com>
2017-03-10 12:04:45 -0500

    add basic routes

--- server.js
+++ server.js
@@ -6,7 +6,28 @@ const app = express()
 app.use(morgan('dev'))
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  console.log(chalk.yellow('Got a GET request for the homepage'))
+  res.send('Hello GET')
+})
+
+app.post('/', (req, res) => {
+  console.log(chalk.yellow('Got a POST request for the homepage'))
+  res.send('Hello POST')
+})
+
+app.delete('/del_user', (req, res) => {
+  console.log(chalk.yellow('Got a DELETE request for /del_user'))
+  res.send('Hello DELETE')
+})
+
+app.get('/list_user', (req, res) => {
+  console.log(chalk.yellow('Got a GET request for /list_user'))
+  res.send('Page Listing')
+})
+
+app.get('/ab*cd', (req, res) => {
+  console.log(chalk.yellow('Got a GET request for /ab*cd'))
+  res.send('Page Pattern Match')
 })
 
 const server = app.listen(8081, _ => {
