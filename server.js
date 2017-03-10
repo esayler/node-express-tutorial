fbeb25b8ed9e95ef5d34311a46b0259887ed0cb0
Eric Sayler <easayler@gmail.com>
2017-03-10 12:47:06 -0500

    add file upload, POST form method

--- server.js
+++ server.js
@@ -2,10 +2,17 @@ import express from 'express'
 import path from 'path'
 import morgan from 'morgan'
 import chalk from 'chalk'
+import fs from 'fs'
+import multer from 'multer'
+import bodyParser from 'body-parser'
+
 const app = express()
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.use(morgan('dev'))
 app.use(express.static('public'))
+
+const upload = multer({ dest: 'uploads/' })
 
 app.get('/', (req, res) => {
   console.log(chalk.yellow('Got a GET request for the homepage'))
@@ -16,15 +23,37 @@ app.get('/index.html', function (req, res) {
   res.sendFile(path.resolve(__dirname, 'index.html'))
 })
 
-app.get('/process_get', (req, res) => {
+app.post('/process_post', urlencodedParser, (req, res) => {
   // Prepare output in JSON format
   const response = {
-    first_name: req.query.first_name,
-    last_name: req.query.last_name,
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
   }
 
   console.log(chalk.green(JSON.stringify(response)))
   res.end(JSON.stringify(response))
+})
+
+app.post('/file_upload', upload.single('file'), (req, res) => {
+  console.log(req.file)
+  const file = path.resolve(__dirname, req.file.originalname)
+
+  fs.readFile(req.file.path, (err, data) => {
+    fs.writeFile(file, data, err => {
+      let response
+      if (err) {
+        response = err
+        console.log(err)
+      } else {
+        response = {
+          message: 'File uploaded successfully',
+          filename: req.file.name,
+        }
+      }
+      console.log(JSON.stringify(response))
+      res.end(JSON.stringify(response))
+    })
+  })
 })
 
 app.post('/', (req, res) => {
